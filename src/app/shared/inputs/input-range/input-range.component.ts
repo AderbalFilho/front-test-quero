47b8ee1a8982f0ddf11a0b2f985d2a87a9dcb49e
AderbalFilho <aderbalcrato@yahoo.com.br>
2019-09-09 06:08:22 -0300

    fix(range): Use of read-only value

--- src/app/shared/inputs/input-range/input-range.component.ts
+++ src/app/shared/inputs/input-range/input-range.component.ts
@@ -24,6 +24,7 @@ export class InputRangeComponent implements OnInit, OnDestroy {
 
   debouncer: Subject<any> = new Subject();
   inputControl: AbstractControl;
+  value: number;
 
   constructor() {
     this.debouncer
@@ -36,6 +37,7 @@ export class InputRangeComponent implements OnInit, OnDestroy {
     this.inputControl = this.form.get(this.key) as AbstractControl;
     this.maxRange = this.maxRange > this.minRange ? this.maxRange : this.minRange;
     this.inputControl.setValue(this.inputControl.value ? this.inputControl.value : this.minRange);
+    this.value = this.inputControl.value;
   }
 
   ngOnDestroy(): void {
@@ -44,6 +46,7 @@ export class InputRangeComponent implements OnInit, OnDestroy {
 
   modelChanged(newValue) {
     if (newValue !== null) {
+      this.inputControl.setValue(this.value);
       this.debouncer.next(newValue);
     }
   }
